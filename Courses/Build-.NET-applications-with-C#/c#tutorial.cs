5eaf685d84b8e14ac7e507e6b2b41cdd44099f8e
Abdelhamid Khamis <53490154+Abdelhamid-khamis@users.noreply.github.com>
2022-07-27 18:25:16 +0200

    Started C# Course, and Explored Jupyter .NET

---
+++ Courses/Build-.NET-applications-with-C#/c#tutorial.cs
@@ -0,0 +1,28 @@
+using System;   // instead of calling `System` everytime before `Console.WriteLine()`
+namespace World
+{
+    public class Hello
+    {
+        public static void Main(string[] args)
+        {
+
+            // Console.WriteLine("Hello World!");
+
+            // This will print Congratulations! You wrote your first lines of code!
+            Console.Write("Congratulations!");
+            Console.Write(" ");
+            Console.Write("You wrote your first lines of code!");
+
+            // Common mistakes new programmers make:
+
+            // Entering lower-case letters instead of capitalizing C in Console, or the letters W or L in WriteLine.
+            // Entering a comma instead of a period between Console and WriteLine.
+            // Forgetting double-quotation marks, or using single-quotation to surround the phrase Hello World!.
+            // Forgetting a semi-colon at the end of the command.
+
+
+        }
+    }
+}
+
+
